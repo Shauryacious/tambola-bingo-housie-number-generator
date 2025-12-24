78537bbf78f9d28c3b924b070487b5d538ffb890
Shauryacious <shauryaagrawal2718@gmail.com>
2025-12-24 18:21:21 +0530

    Fix: Correct NativeWind v4 babel plugin configuration

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
+const reactNativeCssInteropBabel = require('react-native-css-interop/babel');
+
 module.exports = function (api) {
     api.cache(true);
+    const cssInteropConfig = reactNativeCssInteropBabel();
     return {
         presets: ['babel-preset-expo'],
-        plugins: ['nativewind/babel'],
+        plugins: [
+            ...cssInteropConfig.plugins,
+        ],
     };
 };
 
